f14fe7dff2f811ac7c70a65fcc9f4ffadbab73fc
divya-yadv <90103759+divya-yadv@users.noreply.github.com>
2022-06-07 08:40:52 +0530

    updated comments

--- Backtracking/WordBreak.cpp
+++ Backtracking/WordBreak.cpp
@@ -7,7 +7,7 @@ void solve(vector<string>&dict,vector<string>&res,string s,string p,int i)
             return;
         }
         string str="";
-        for(int k=i;k<s.length();k++) // loop will run to get all different combinations
+        for(int k=i;k<s.length();k++) // loop will run to get all different combinations with the same index if possible thats how we get all answers
         {
             str += s[k];
             if(find(dict.begin(),dict.end(),str) !=  dict.end()) // to check if current word is present
