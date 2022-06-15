8251d0cb816e3b74b8debcb1ffe2614f283944f2
Divya Yadav <90103759+divya-yadv@users.noreply.github.com>
2022-06-15 13:07:29 +0530

    create nCr file

---
+++ Dynamic Programming/nCr.cpp
@@ -0,0 +1,34 @@
+# recursion
+
+int binomialCoeff(int n, int k)
+{
+    // Base Cases
+    if (k > n)
+        return 0;
+    if (k == 0 || k == n)
+        return 1;
+ 
+    // Recur
+    return binomialCoeff(n - 1, k - 1)
+           + binomialCoeff(n - 1, k);
+}
+
+#dp
+
+int nCr(int n, int r){
+        int dp[n+1][n+1];
+        if(n<r){
+           return 0;
+        }
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<i+1; j++){ //takes care of r>n
+                if(j == 0 || i == j){
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = ((dp[i-1][j-1])%1000000007+(dp[i-1][j])%1000000007)%1000000007;
+                }
+            }
+        }
+        return dp[n][r];
+    }
